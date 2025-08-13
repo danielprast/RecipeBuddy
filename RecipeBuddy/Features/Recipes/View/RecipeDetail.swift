5836ea38c2972af4c7d44f82fe9af3173c6e1ed5
Daniel <danielprastiwa@gmail.com>
2025-08-13 22:49:43 +0700

    🏗️ Develop toggle favorited recipe toggle logic from detail recipe view

--- RecipeBuddy/Features/Recipes/View/RecipeDetail.swift
+++ RecipeBuddy/Features/Recipes/View/RecipeDetail.swift
@@ -15,9 +15,36 @@ struct RecipeDetail: View {
 
   @ObservedObject var recipeData: RecipeViewModel
 
+  var isFavorite: Bool {
+    guard let recipeDetail = recipeData.detailRecipe else {
+      return false
+    }
+    return recipeData.checkFavorite(recipeDetail)
+  }
+
   var body: some View {
     VStack {
       Text("Detail Recipe")
+      Button {
+        guard let recipeDetail = recipeData.detailRecipe else {
+          return
+        }
+        recipeData.handleFavorite(recipeDetail)
+      } label: {
+        if isFavorite {
+          Label(
+            "Remove Favorite",
+            systemImage: "star.circle.fill"
+          )
+          .foregroundStyle(Color.blue)
+        } else {
+          Label(
+            "Add to Favorite",
+            systemImage: "star.circle.fill"
+          )
+          .foregroundStyle(Color.gray)
+        }
+      }
     }
     .navigationTitle(recipeData.detailRecipeTitle)
   }
